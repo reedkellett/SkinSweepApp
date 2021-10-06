96ac820a3422c10cac72a93e61e7b350e90cc151
Reed Kellett <reed.kellett@gmail.com>
2021-10-05 22:12:34 -0400

    added test data and views to dashboard and profile screens

--- screens/DashboardScreen.tsx
+++ screens/DashboardScreen.tsx
@@ -1,19 +1,36 @@
 import * as React from "react";
-import { StyleSheet } from "react-native";
+import { FlatList, StyleSheet } from "react-native";
 
 import { Text, View } from "../components/Themed";
 import { RootTabScreenProps } from "../types";
+
+const dataSource = [
+  { key: "1", text: "Entry 1" },
+  { key: "2", text: "Entry 2" },
+  { key: "3", text: "Entry 3" },
+  { key: "4", text: "Entry 4" },
+  { key: "5", text: "Entry 5" },
+  { key: "6", text: "Entry 6" },
+  { key: "7", text: "Entry 7" },
+  { key: "8", text: "Entry 8" },
+  { key: "9", text: "Entry 9" },
+];
 
 export default function DashboardScreen({
   navigation,
 }: RootTabScreenProps<"Dashboard">) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Dashboard</Text>
+      <Text style={styles.title}>Overview Information</Text>
       <View
         style={styles.separator}
         lightColor="#eee"
         darkColor="rgba(255,255,255,0.1)"
+      />
+      <FlatList
+        data={dataSource}
+        renderItem={({ item }) => <Text>{item.text}</Text>}
+        numColumns={3}
       />
     </View>
   );
--- screens/ProfileScreen.tsx
+++ screens/ProfileScreen.tsx
@@ -1,18 +1,46 @@
 import * as React from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet, Image, FlatList } from "react-native";
 
 import { Text, View } from "../components/Themed";
 
+const name = "Reed Kellett";
+
+const profileData = [
+  { key: "Height", value: 69 },
+  { key: "Weight", value: 156 },
+  { key: "Sex", value: "Male" },
+  { key: "Date of Birth", value: "Jun 12, 1999 (22)" },
+  { key: "Fitzpatrick Skin Type", value: "Not Set" },
+  { key: "Email", value: "reed.kellett@gmail.com" },
+];
+
 export default function ProfileScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Profile</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
+    <View style={{ flexDirection: "column", alignItems: "center" }}>
+      <Image
+        style={{
+          height: 60,
+          width: 60,
+          backgroundColor: "grey",
+          borderRadius: 50,
+        }}
+        source={require("../assets/images/icon.png")}
+      />
+      <Text>{name}</Text>
+      <FlatList
+        data={profileData}
+        renderItem={({ item }) => <Text>{`${item.key}: ${item.value}`}</Text>}
       />
     </View>
+
+    // <View style={styles.container}>
+    //   <Text style={styles.title}>Profile</Text>
+    //   <View
+    //     style={styles.separator}
+    //     lightColor="#eee"
+    //     darkColor="rgba(255,255,255,0.1)"
+    //   />
+    // </View>
   );
 }
 
