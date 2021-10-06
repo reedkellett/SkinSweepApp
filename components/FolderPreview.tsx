44a13d8761e4bb024222fd2333da959c3a7da92f
Reed Kellett <reed.kellett@gmail.com>
2021-10-06 16:38:22 -0400

    updated folderPreview, created folderScreen, updated linking

--- components/FolderPreview.tsx
+++ components/FolderPreview.tsx
@@ -1,30 +1,36 @@
 import React from "react";
-import { View, Image, Text } from "react-native";
+import { View, Image, Text, TouchableHighlight } from "react-native";
 
 const folderName = "Left Elbow Mole";
 const priority = 1;
 
 export default function FolderPreview() {
+  const openFolder = () => {
+    console.log("open Folder pressed");
+  };
+
   return (
-    <View
-      style={{
-        flexDirection: "column",
-        alignItems: "center",
-        borderWidth: 5,
-        borderColor: "red",
-        padding: 5,
-      }}
-    >
-      <Text style={{}}>{folderName}</Text>
-      <Image
+    <TouchableHighlight onPress={() => openFolder()}>
+      <View
         style={{
-          height: 60,
-          width: 60,
-          backgroundColor: "grey",
-          borderRadius: 50,
+          flexDirection: "column",
+          alignItems: "center",
+          borderWidth: 5,
+          borderColor: "red",
+          padding: 5,
         }}
-        source={require("../assets/images/icon.png")}
-      />
-    </View>
+      >
+        <Text style={{}}>{folderName}</Text>
+        <Image
+          style={{
+            height: 60,
+            width: 60,
+            backgroundColor: "grey",
+            borderRadius: 50,
+          }}
+          source={require("../assets/images/icon.png")}
+        />
+      </View>
+    </TouchableHighlight>
   );
 }
