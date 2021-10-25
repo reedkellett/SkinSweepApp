287ee4802a2a91acaa558768763562122b839bd1
Reed Kellett <reed.kellett@gmail.com>
2021-10-25 15:25:59 -0400

    Updated profile screen

--- screens/ProfileScreen.tsx
+++ screens/ProfileScreen.tsx
@@ -1,58 +1,209 @@
-import * as React from "react";
-import { StyleSheet, Image, FlatList } from "react-native";
+import React, { useState, useReducer } from "react";
+import { StyleSheet, Image, TextInput, Button } from "react-native";
+import RNPickerSelect from "react-native-picker-select";
 
 import { Text, View } from "../components/Themed";
 
-const name = "Reed Kellett";
-
-const profileData = [
-  { key: "Height", value: 69 },
-  { key: "Weight", value: 156 },
-  { key: "Sex", value: "Male" },
-  { key: "Date of Birth", value: "Jun 12, 1999 (22)" },
-  { key: "Fitzpatrick Skin Type", value: "Not Set" },
-  { key: "Email", value: "reed.kellett@gmail.com" },
-];
+const nameBE = "Reed Kellett ";
+const emailBE = "reed.kellett@gmail.com";
+const heightBE = "70";
+const weightBE = "160";
+const sexBE = "male";
+const DOBBE = "1999-06-12";
+const fitzTypeBE = "IV";
 
 export default function ProfileScreen() {
-  return (
-    <View style={{ flexDirection: "column", alignItems: "center" }}>
-      <Image
-        style={{
-          height: 60,
-          width: 60,
-          backgroundColor: "grey",
-          borderRadius: 50,
-        }}
-        source={require("../assets/images/icon.png")}
-      />
-      <Text>{name}</Text>
-      <FlatList
-        data={profileData}
-        renderItem={({ item }) => <Text>{`${item.key}: ${item.value}`}</Text>}
-      />
-    </View>
+  const [name, setName] = useState(nameBE);
+  const [email, setEmail] = useState(emailBE);
+  const [height, setHeight] = useState(heightBE);
+  const [weight, setWeight] = useState(weightBE);
+  const [sex, setSex] = useState(sexBE);
+  const [fitzType, setFitzType] = useState(fitzTypeBE);
+  const [DOB, setDOB] = useState(DOBBE);
+  const [editMode, setEditMode] = useState(false);
 
-    // <View style={styles.container}>
-    //   <Text style={styles.title}>Profile</Text>
-    //   <View
-    //     style={styles.separator}
-    //     lightColor="#eee"
-    //     darkColor="rgba(255,255,255,0.1)"
-    //   />
-    // </View>
+  // const cancel = () => {
+  //   setEditMode(false);
+  //   console.log("cancel");
+  // };
+  const edit = () => {
+    setEditMode(true);
+  };
+  const done = () => {
+    // send updated info to BE
+    setEditMode(false);
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.buttonsContainer}>
+        {/* <Button onPress={cancel} title="Cancel" disabled={!editMode} /> */}
+        <Button
+          onPress={editMode ? done : edit}
+          title={editMode ? "Done" : "Edit"}
+          disabled={false}
+        />
+      </View>
+
+      <View style={styles.header}>
+        <Image
+          style={styles.profileImage}
+          source={require("../assets/images/icon.png")}
+        />
+        <TextInput
+          style={styles.name}
+          onChangeText={(name) => setName(name)}
+          value={name}
+          editable={false}
+        />
+        <TextInput
+          style={styles.primaryUserInfo}
+          onChangeText={(email) => setEmail(email)}
+          value={email}
+          editable={false}
+        />
+      </View>
+
+      <View style={styles.body}>
+        <View style={styles.listContainer}>
+          <Text>Height (in):</Text>
+          <TextInput
+            style={
+              editMode
+                ? styles.secondaryUserInfoEditMode
+                : styles.secondaryUserInfo
+            }
+            onChangeText={(height) => setHeight(height)}
+            value={height}
+            editable={editMode}
+          />
+        </View>
+
+        <View style={styles.listContainer}>
+          <Text>Weight (lb):</Text>
+          <TextInput
+            style={
+              editMode
+                ? styles.secondaryUserInfoEditMode
+                : styles.secondaryUserInfo
+            }
+            onChangeText={(weight) => setWeight(weight)}
+            value={weight}
+            editable={editMode}
+          />
+        </View>
+
+        <View style={styles.listContainer}>
+          <Text>Sex:</Text>
+          <RNPickerSelect
+            style={editMode ? pickerSelectStylesEditMode : pickerSelectStyles}
+            placeholder={{ label: "Not Set", value: null }}
+            onValueChange={(sex) => setSex(sex)}
+            items={[
+              { label: "Male", value: "male" },
+              { label: "Female", value: "female" },
+              { label: "Other", value: "other" },
+            ]}
+            disabled={!editMode}
+            value={sex}
+          />
+        </View>
+
+        <View style={styles.listContainer}>
+          <Text>Date of Birth:</Text>
+          <TextInput
+            style={
+              editMode
+                ? styles.secondaryUserInfoEditMode
+                : styles.secondaryUserInfo
+            }
+            onChangeText={(DOB) => setDOB(DOB)}
+            value={DOB}
+            editable={editMode}
+          />
+        </View>
+
+        <View style={styles.listContainer}>
+          <Text>FitzPatrick Skin Type:</Text>
+          <RNPickerSelect
+            style={editMode ? pickerSelectStylesEditMode : pickerSelectStyles}
+            placeholder={{ label: "Not Set", value: null }}
+            onValueChange={(fitzType) => setFitzType(fitzType)}
+            items={[
+              { label: "I", value: "I" },
+              { label: "II", value: "II" },
+              { label: "III", value: "III" },
+              { label: "IV", value: "IV" },
+              { label: "V", value: "V" },
+              { label: "VI", value: "VI" },
+            ]}
+            disabled={!editMode}
+            value={fitzType}
+          />
+        </View>
+      </View>
+    </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+const pickerSelectStyles = StyleSheet.create({
+  inputIOS: {
+    color: "black",
   },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
+  inputAndriod: {
+    color: "black",
+  },
+});
+
+const pickerSelectStylesEditMode = StyleSheet.create({
+  inputIOS: {
+    color: "blue",
+  },
+  inputAndriod: {
+    color: "blue",
+  },
+});
+
+const styles = StyleSheet.create({
+  body: {
+    //backgroundColor: "green",
+  },
+  buttonsContainer: {
+    flexDirection: "row-reverse",
+    // justifyContent: "space-between",
+    //backgroundColor: "#F9C9C9",
+  },
+  container: {
+    flexDirection: "column",
+  },
+  header: {
+    //backgroundColor: "#F9C9C9",
+    alignItems: "center",
+  },
+  listContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: "5%",
+  },
+  name: {
+    fontSize: 22,
+    color: "#000000",
+  },
+  primaryUserInfo: {
+    fontSize: 16,
+    color: "#778899",
+  },
+  profileImage: {
+    height: 100,
+    width: 100,
+    //backgroundColor: "grey",
+    borderRadius: 50,
+  },
+  secondaryUserInfo: {
+    color: "black",
+  },
+  secondaryUserInfoEditMode: {
+    color: "blue",
   },
   separator: {
     marginVertical: 30,
